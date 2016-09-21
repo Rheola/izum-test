029f25f989118f795f82180565b870f75751a1d4
Rheola <okunewa@yandex.ru>
2016-09-22 01:56:00 +0300

    bootstrap

---
+++ application/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/core/DbRecord.php';
+require_once __DIR__.'/core/Database.php';
+require_once __DIR__.'/core/Controller.php';
+require_once __DIR__.'/core/SiteError.php';
+require_once __DIR__.'/core/App.php';
+App::start();
