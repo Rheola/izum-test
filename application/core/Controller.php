8541420a81061463f5a1103cfbe036e75a2300f2
Rheola <okunewa@yandex.ru>
2016-09-22 01:54:32 +0300

    PHPdoc

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class Controller
+ */
 class Controller{
 
     public $model;
@@ -10,22 +13,33 @@ class Controller{
 
     private $_id;
 
-    public $user;
-
+    /**
+     * Controller constructor.
+     */
     public function __construct(){
         $name = get_class($this);
-        $this->user = new WebUser();
         $this->_id = strtolower(substr($name, 0, -strlen('Controller')));
     }
 
+    /**
+     *
+     */
     public function actionIndex(){
         $this->render($this->_id.'/index');
     }
 
+    /**
+     * @param $view
+     * @param null $data
+     */
     public function render($view, $data = null){
         $this->generate($this->_id.'/'.$view, $data);
     }
 
+    /**
+     * @param $template
+     * @param null $data
+     */
     private function generate($template, $data = null){
 
         if(is_array($data)){
