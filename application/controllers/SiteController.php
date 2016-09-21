c413a53c982d619f6b174e589913b1abc8d85e90
Rheola <okunewa@yandex.ru>
2016-09-22 01:57:20 +0300

    Переключение языка.

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -1,7 +1,20 @@
 <?php
 
 class SiteController extends Controller{
+
     public function actionError(SiteError $error){
         $this->render('error', ['error' => $error]);
     }
+
+    public function actionLanguage(){
+        $val = 'ru';
+        if(App::getLanguage() === 'ru'){
+            $val = 'en';
+        }
+        $site = $_SERVER['HTTP_HOST'];
+        setcookie('language', $val, time() + App::SecondInDay, '/', $site);
+        header('Location: /', true);
+        exit;
+    }
+
 }
